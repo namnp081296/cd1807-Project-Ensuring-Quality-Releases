51f8c12d589dc1a0e1accdedbe0d4cfbee368763
Nam Nguyen Phuong <namnp081296@gmail.com>
2023-04-25 09:10:21 +0700

    Deploy Resource

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -10,11 +10,12 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S')
 
 options = ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox") 
+options.add_argument("--no-sandbox")
+options.add_argument("--headless") 
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu") 
-options.add_argument("--disable-extensions") 
+options.add_argument("--disable-extensions")
+options.add_argument('--remote-debugging-port=9999')
 driver = webdriver.Chrome(options=options)
 
 # print ('INFO: Browser started successfully. Navigating to the demo page to login.')
